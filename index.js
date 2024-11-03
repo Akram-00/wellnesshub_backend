3679803b4785baa24eeabf34c61f267fcbdaed4c
Akram-00 <aliakram9789@gmail.com>
2024-11-03 14:43:11 +0530

    NO ACCESS HEADER IS ADDED

--- index.js
+++ index.js
@@ -49,6 +49,24 @@ const corsOptions = {
 // CORS Middleware
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+  
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+  
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+      return res.status(200).json({});
+    }
+  
+    next();
+  });
+  
+
 
 // storing your cookies
 app.use(cookieParser());
